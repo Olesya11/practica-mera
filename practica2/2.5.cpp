578d571679ed2a26d3c8a17572e45a13f821279e
Olesya11 <olesyagolovanova1@yandex.ru>
2016-11-18 08:53:38 +0300

    Add files via upload

---
+++ practica2/2.5.cpp
@@ -0,0 +1,47 @@
+// 2.5.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define n 9
+#define m 11
+int main()
+{
+	
+	char  s[n];
+	int a = 10, q;
+	srand(time(NULL));
+	for (int j = 1; j < m; j++) {
+		for (int i = 0; i < n; i++)
+		{
+		q = rand() % 3;
+			switch (q)
+		{
+			case 0:
+
+				s[i] = rand() % 26 + 'A';
+				break;
+
+			case 1:
+
+				s[i] = rand() % 26 + 'a';
+				break;
+
+			case 2:
+
+				s[i] = rand() % 10 + '0';
+				break;
+		}
+		s[n-1]='\0';
+		}
+		printf("%s\n", s);
+		
+	                    
+  }
+
+
+getchar();
+return 0;
+}
+
