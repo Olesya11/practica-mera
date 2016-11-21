b2ad96aa2e258ed4813e4830609a946c75672bd2
Olesya11 <olesyagolovanova1@yandex.ru>
2016-11-21 21:53:20 +0300

    Add files via upload

---
+++ practica3/3.1.cpp
@@ -0,0 +1,31 @@
+// 3.1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "stdio.h"
+#define n 40
+
+int main()
+{
+	char s[n];
+   int word, count=0;
+   printf("Enter the string: \n");
+   fgets(s, n, stdin);
+   int i=0;
+   word = 0;
+   while(s[i] !='\0')
+   {
+	   if(s[i]!=' ' && word == 0)
+	   {
+		   word = 1;
+		   count++;
+	   }
+	   else if(s[i]==' ')
+      word = 0;
+    i++;
+  }
+  printf("The number of words in the string = %d ", count);
+ getchar();
+	return 0;
+}
+
